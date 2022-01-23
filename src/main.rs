be8e9ab9e4dca79d28cc305199db1a5b51ae6487
anawesumoposum <ericnsibley@gmail.com>
2022-01-23 15:16:48 -0500

    chapter 13 updates

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    //pass ownership of the iterator to constructor
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
